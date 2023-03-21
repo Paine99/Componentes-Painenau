2d79eb379741fcb61c7d0441e5a97c9f0627b765
Leonardo Painenau <lamuraya2010@hotmail.com>
2023-03-20 21:50:52 -0300

    para entregar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { AsideComponent } from './aside/aside.component';
+import { MainComponent } from './main/main.component';
+import { ContenedorComponent } from './contenedor/contenedor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    AsideComponent,
+    MainComponent,
+    ContenedorComponent
   ],
   imports: [
     BrowserModule
